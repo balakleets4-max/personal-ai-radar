b38f4cfbca2c0009995d4340e5a0251b7a70984b
balakleets4-max <balakleets4@gmail.com>
2026-05-31 23:34:36 +0300

    Schedule calendar background checks on app start

--- src/main/kotlin/com/personalradar/app/PersonalAiRadarApplication.kt
+++ src/main/kotlin/com/personalradar/app/PersonalAiRadarApplication.kt
@@ -1,5 +1,11 @@
 package com.personalradar.app
 
 import android.app.Application
+import com.personalradar.app.calendar.CalendarBackgroundScheduler
 
-class PersonalAiRadarApplication : Application()
+class PersonalAiRadarApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        CalendarBackgroundScheduler(applicationContext).ensureScheduled()
+    }
+}
