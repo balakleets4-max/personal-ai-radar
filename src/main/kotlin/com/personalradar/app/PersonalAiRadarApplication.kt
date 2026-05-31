c1393e1283c412595c223fefee821b9e9ed43606
balakleets4-max <balakleets4@gmail.com>
2026-06-01 00:58:12 +0300

    Restart persistent watch when enabled

--- src/main/kotlin/com/personalradar/app/PersonalAiRadarApplication.kt
+++ src/main/kotlin/com/personalradar/app/PersonalAiRadarApplication.kt
@@ -3,11 +3,16 @@ package com.personalradar.app
 import android.app.Application
 import com.personalradar.app.calendar.CalendarBackgroundScheduler
 import com.personalradar.app.calendar.CalendarChangeObserverRegistry
+import com.personalradar.app.reliability.BackgroundReliabilityStore
+import com.personalradar.app.reliability.PersistentWatchService
 
 class PersonalAiRadarApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         CalendarBackgroundScheduler(applicationContext).ensureScheduled()
         CalendarChangeObserverRegistry.ensureRegistered(applicationContext)
+        if (BackgroundReliabilityStore(applicationContext).isPersistentWatchEnabled()) {
+            PersistentWatchService.start(applicationContext)
+        }
     }
 }
