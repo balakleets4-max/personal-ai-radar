aee3fdf6d647398a0bfb5257811100c4da575229
balakleets4-max <balakleets4@gmail.com>
2026-06-01 00:06:14 +0300

    Register calendar observer on app start

--- src/main/kotlin/com/personalradar/app/PersonalAiRadarApplication.kt
+++ src/main/kotlin/com/personalradar/app/PersonalAiRadarApplication.kt
@@ -2,10 +2,12 @@ package com.personalradar.app
 
 import android.app.Application
 import com.personalradar.app.calendar.CalendarBackgroundScheduler
+import com.personalradar.app.calendar.CalendarChangeObserverRegistry
 
 class PersonalAiRadarApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         CalendarBackgroundScheduler(applicationContext).ensureScheduled()
+        CalendarChangeObserverRegistry.ensureRegistered(applicationContext)
     }
 }
